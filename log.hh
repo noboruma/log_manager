5bdab72a30a32b9872ba81a8f087e8647376f348
Thomas Legris <legris.thomas@gmail.com>
2015-04-14 10:47:18 -0400

    Improve log manager

--- log.hh
+++ log.hh
@@ -10,40 +10,56 @@
 #include <sstream> // stringstream
 #include <iomanip> // put_time
 #include <string>
+#include <stdexcept>
+#include <fstream>
+#include <list>
+#include <fstream>
 
 namespace global {
 
-  template<class out>
-  struct log
+  namespace log
   {
     enum class level
     {
-      DEBUG,
-      VERBOSE
+      DBG,
+      ERR,
+      WRN,
+      NFO,
+      VRB
     };
 
-    template<level l=level::VERBOSE>
-    static void print(const std::string &who, const std::string &msg)
+    static std::list<std::ofstream*> logs;
+    static bool verbose = false;
+
+    std::ostream& operator<<(std::ostream& o, log::level l)
     {
-      std::cout<<"["<<who<<"] "<<log::get_now()<< " (INFO) " <<msg<<std::endl;
+      switch(l)
+      {
+        case level::DBG:
+          o<<" (DBG) ";
+          break;
+        case level::ERR:
+          o<<" (ERR) ";
+          break;
+        case level::WRN:
+          o<<" (WRN) ";
+          break;
+        case level::NFO:
+          o<<" (NFO) ";
+          break;
+        case level::VRB:
+          o<<" (VRB) ";
+          break;
+      }
+      return o;
     }
 
-    //template<>
-    //static void operator()(const std::string &who, const std::string &msg)
-    //{
-    //  out<<"["<<who<<"] "<<log::get_now()<< " (DEBUG) " <<msg<<std::endl;
-    //}
-
-
-    static std::string get_now()
+    std::string get_now()
     {
       auto now = std::chrono::system_clock::now();
       auto in_time_t = std::chrono::system_clock::to_time_t(now);
       auto duration = now.time_since_epoch();
-      auto duration_minus_one = now - std::chrono::seconds(1);
-      auto millis1 = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
-      auto millis2 = std::chrono::duration_cast<std::chrono::milliseconds>(duration_minus_one.time_since_epoch()).count();
-      auto millis = millis1 -millis2;
+      auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() % 1000;
 
       std::stringstream ss;
       char format_date [100];
@@ -53,12 +69,81 @@ namespace global {
                     std::localtime(&in_time_t)); 
 
       ss<<format_date<<":"<<millis;
-      
+
       return ss.str();
     }
+
+    void init(bool v = false)
+    { 
+      verbose = v;
+    }
+
+    std::list<std::ofstream*>::iterator attach_log(const std::string &s)
+    {
+      logs.push_back(new std::ofstream(s));
+      auto last_it = logs.end();
+      --last_it;
+      return last_it;
+    }
+
+    void detach_log(std::list<std::ofstream*>::iterator it)
+    {
+      delete *it;
+      logs.erase(it);
+    }
+
+    void clean()
+    {
+      for(std::list<std::ofstream*>::iterator it = logs.begin();
+          it != logs.end();
+          ++it)
+        delete *it;
+       logs.clear();
+    }
+
+
+    template<log::level l>
+    inline void trace(const std::string &who, const std::string &msg)
+    {
+
+      std::stringstream ss;
+      ss <<"["<<who<<"] "<<get_now()<< l <<msg;
+      std::cout << ss.str() << std::endl;
+      for(auto& log : logs)
+        (*log)<< ss.str() <<std::endl;
+    }
+
+    template<>
+    inline void trace<level::ERR>(const std::string &who, const std::string &msg)
+    {
+      std::stringstream ss;
+      ss <<"["<<who<<"] "<<get_now()<< level::ERR <<msg;
+      std::cerr<< ss <<std::endl;;
+      for(auto& log : logs)
+        (*log)<< ss <<std::endl;;
+    }
+
+    template<>
+    inline void trace<level::VRB>(const std::string &who, const std::string &msg)
+    {
+      if(!verbose)
+        return;
+
+      std::stringstream ss;
+      ss << "["<<who<<"] "<<get_now()<< level::VRB <<msg;
+      std::cout << ss.str() << std::endl;
+      for(auto& log : logs)
+        (*log)<< ss.str() << std::endl;
+    }
     
-  };
-  
+#ifdef NDEBUG
+    template<>
+    inline void trace<log::level::DBG>(const std::string &who,
+                                       const std::string &msg)
+    {}
+#endif
+
+  }//! log
 } //!global
 
 #endif
--- main.cc
+++ main.cc
@@ -3,8 +3,14 @@
 
 int main()
 {
-  global::log<int>::print("main", "start");
-  global::log<int>::print("main", "end");
+  global::log::init(true);
+
+  auto it = global::log::attach_log("toto.log");
+
+  global::log::trace<global::log::level::DBG>("main", "start");
+  global::log::trace<global::log::level::VRB>("main", "end");
+
+  global::log::clean();
 
   return 0;
 }
